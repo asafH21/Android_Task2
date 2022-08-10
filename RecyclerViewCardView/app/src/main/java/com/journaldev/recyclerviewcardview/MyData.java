ac3f068232cf556abe0492a5e53ebaf0b6091bcd
asafH21 <asafhermann@gmail.com>
2022-08-10 22:27:42 +0300

    added recycler folder

---
+++ RecyclerViewCardView/app/src/main/java/com/journaldev/recyclerviewcardview/MyData.java
@@ -0,0 +1,15 @@
+package com.journaldev.recyclerviewcardview;
+
+
+
+public class MyData {
+
+    static String[] nameArray = {"Bulbasaur", "Charmander", "Squirtle", "Caterpie", "Weedle", "Pidgey", "Rattata","Spearow", "Ekans", "Pichu", "Sandshrew"};
+    static String[] versionArray = {"Pokemon type Grass", "Pokemon type Fire", "Pokemon type Water", "Pokemon type Bug", "Pokemon type Poison", "Pokemon type Flying", "Pokemon type Normal", "Pokemon type Flying", "Pokemon type Poison", "Pokemon type Electric","Pokemon type Ground"};
+
+    static Integer[] drawableArray = {R.drawable.bulbasaur, R.drawable.charmander, R.drawable.squirtle,
+            R.drawable.caterpie, R.drawable.weedle, R.drawable.pidgey, R.drawable.rattata,
+            R.drawable.spearow, R.drawable.ekans, R.drawable.pichu,R.drawable.sandshrew};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
